262bebcb5372dba41d343a24dbf4301ba73a9d48
Anhkhoi Vu-Nguyen <anhkhoi.vunguyen@unity3d.com>
2024-11-01 16:29:48 -0400

    fix typos

--- Basic/Basic AssetReference/postbuild.sh
+++ Basic/Basic AssetReference/postbuild.sh
@@ -5,10 +5,11 @@ echo "Running post build..."
 echo "$WORKSPACE/.build/last/$BUILD_TARGET/extra_data/addrs"
 
 # Define the path where the addressables should be copied
-TARGET_DIR="$WORKSPACE/.build/last/$BUILD_TARGET/extra_data/addrs
+TARGET_DIR="$WORKSPACE/.build/last/$BUILD_TARGET/extra_data/addrs"
 
 # Ensure the target directory exists
-mkdir -p "$WORKSPACE/.build/last/$BUILD_TARGET/extra_data/addrs
+mkdir -p "$TARGET_DIR"
+echo "Target directory for addressables: $TARGET_DIR";
 
 # Generate a test file (or replace with your actual addressables)
 echo "This is a test file for post-build addressables." > test_addressable.txt
